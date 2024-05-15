e61d6a863428e4f7e25b52eb20545331f5d499bc
eimybarrera <eimybarrera2019@gmail.com>
2024-05-14 22:53:11 -0500

    Creo la carpeta Componentes y el componente de interfaz

---
+++ tradetrove/src/componentes/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../estilos-componentes/NavBar.css"
+function NavBar(){
+    return(
+        <div class="navbar">
+            <button class="nav-button">Sobre nosotros</button>
+        </div>
+        
+    )
+}
+export default NavBar;
