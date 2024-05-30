16ecc6977a921f3c07dc00b86a9b6a31fc9570ca
ValtinaA <vale2206arias@gmail.com>
2024-05-30 14:47:33 -0500

    Back_end de Asesor y Inventario listo.

--- tradetrove-back/src/routes/auth.routes.js
+++ tradetrove-back/src/routes/auth.routes.js
@@ -1,10 +1,12 @@
-import {Router} from 'express'
-import { Register,Iniciar} from '../controller/Validation.js';
+import { Router } from 'express';
+import { Asesor, Inventario } from '../controller/Servicios.js';
+import { Iniciar, Register } from '../controller/Validation.js';
 
 const auth = Router();
 
+auth.post('/Registrar', Register);
+auth.get('/iniciar', Iniciar);
+auth.get('/asesor', Asesor);
+auth.get('/inventario', Inventario);
 
-auth.post('/Registrar', Register)
-auth.get('/iniciar', Iniciar)
-
-export default auth
+export default auth;
